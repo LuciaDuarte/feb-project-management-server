450cc9c44bd931227a1d2cf77cf62cb0e4772d98
Lúcia Duarte <analuciacfduarte@gmail.com>
2024-02-19 21:34:11 +0000

    clean app.js

--- app.js
+++ app.js
@@ -33,6 +33,4 @@ app.use('/auth', authRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
-console.log('re-adding stuff');
-
 module.exports = app;
